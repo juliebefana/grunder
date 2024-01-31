6e23d36c983f8bd35e4c8dbac3a52071db38b02d
juliebefana <marcusjuliahiller@gmail.com>
2024-01-31 19:18:50 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter some text: ");
+
+        string userInput = Console.ReadLine();
+
+        Console.WriteLine("You entered: " + userInput);
+
+        Console.Write("Enter the first number: ");
+
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double sum = num1 + num2;
+
+        Console.WriteLine($"The sum of {num1} and {num2} is: {sum}");
+    }
+}
+
